1fcdaf446837f88b06d8c6a33dd0fa1b2149bba9
enrycoop <enrycoop92@gmail.com>
2026-04-29 21:05:56 +0200

    indovina il numero finito

--- gioco_indovinello/src/main.rs
+++ gioco_indovinello/src/main.rs
@@ -1,15 +1,42 @@
+// module cmp, enum Ordering
+use std::cmp::Ordering;
+// module io
 use std::io;
+
+// trait Rng per poter usare tutti quelli che implementano il trait Rng
+use rand::Rng;
 
 fn main() {
     println!("Indovina il numero!");
 
-    println!("Inserisci la tua ipotesi.");
+    let numero_segreto = rand::thread_rng().gen_range(1..=100);
 
-    let mut ipotesi = String::new();
+    println!("Il numero segreto è: {numero_segreto}");
 
-    io::stdin()
-        .read_line(&mut ipotesi)
-        .expect("Errore di lettura");
+    loop {
+        println!("Inserisci la tua ipotesi.");
 
-    println!("Hai ipotizzato: {ipotesi}");
+        let mut ipotesi = String::new();
+
+        io::stdin()
+            .read_line(&mut ipotesi)
+            .expect("Errore di lettura");
+
+        // shadowing di ipotesi, cambio del tipo
+        let ipotesi: u32 = match ipotesi.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Hai ipotizzato: {ipotesi}");
+
+        match ipotesi.cmp(&numero_segreto) {
+            Ordering::Less => println!("Troppo piccolo!"),
+            Ordering::Greater => println!("Troppo Grande!"),
+            Ordering::Equal => {
+                println!("Hai indovinato!");
+                break;
+            }
+        }
+    }
 }
